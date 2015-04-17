f48976712a9f3713c6c0b6ff912499e1c37ec0a7
Ajay Gandhi <aag255@cornell.edu>
2015-04-17 16:56:46 -0400

    Enable single board game

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 
 var blessed = require('blessed');
+var Board = require('./board');
+
+var game = new Board();
+
+var o_turn = false;
+var cursor_position = {
+  board: 0,
+  row: 0,
+  col: 0
+}
 
 var screen = blessed.screen({
   autoPadding: true,
@@ -63,9 +73,6 @@ var cursor = blessed.box({
   width: 1,
   height: 1,
   tags: true,
-  // border: {
-  //   type: 'line'
-  // },
   style: {
     fg: 'white',
     bg: 'black'
@@ -73,9 +80,6 @@ var cursor = blessed.box({
 });
 
 container.append(cursor);
-
-cursor.top = 0;
-screen.render();
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
@@ -86,48 +90,117 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
 // Because of padding in container, get methods are offset
 screen.key('down', function(ch, key) {
   var t = cursor.top - 2;
+  cursor.style.fg = 'white';
+
   if (t < 18) {
     if (t == 4 || t == 11) {
+      cursor_position.board += 3;
+      cursor_position.row = 0;
+
       t++;
     }
+    cursor_position.row++;
     cursor.top = t + 2;
   }
 
+  // console.log(cursor.top + ', ' + cursor.left);
   screen.render();
 });
 
 screen.key('up', function(ch, key) {
   var t = cursor.top - 2;
+  cursor.style.fg = 'white';
+
   if (t > 0) {
     if (t == 7 || t == 14) {
+      cursor_position.board -= 3;
+      cursor_position.row = 2;
+
       t--;
     }
+    cursor_position.row--;
     cursor.top = t - 2;
   }
 
+  // console.log(cursor.top + ', ' + cursor.left);
   screen.render();
 });
 
 screen.key('left', function(ch, key) {
   var l = cursor.left - 5;
+  cursor.style.fg = 'white';
+
   if (l > 0) {
     if (l == 14 || l == 28) {
+      cursor_position.board--;
+      cursor_position.col = 2;
+
       l -= 2;
     }
+    cursor_position.col--;
     cursor.left = l - 3;
   }
 
+  // console.log(cursor.top + ', ' + cursor.left);
   screen.render();
 });
 
 screen.key('right', function(ch, key) {
+  cursor.style.fg = 'white';
+
   var l = cursor.left - 5;
-  console.log(l);
   if (l < 36) {
     if (l == 8 || l == 22) {
+      cursor_position.board++;
+      cursor_position.col = 0;
       l += 2;
     }
+    cursor_position.col++;
     cursor.left = l + 5;
+  }
+
+  // console.log(cursor.top + ', ' + cursor.left);
+  screen.render();
+});
+
+screen.key('space', function(ch, key) {
+  var success = game.make_move(cursor_position.board, cursor_position.row, cursor_position.col, (o_turn) ? 'x' : 'o');
+
+  // Move was possible, i.e. spot empty
+  if (success) {
+    var played = blessed.box({
+      content: (o_turn) ? 'x' : 'o',
+      top: cursor.top - 2,
+      left: cursor.left - 4,
+      width: 1,
+      height: 1
+    });
+
+    o_turn = !o_turn;
+
+    container.append(played);
+    cursor.setFront();
+
+    var ended = game.won_board(cursor_position.board);
+    console.log(ended);
+
+    if (ended !== 'undetermined') {
+
+      var finished = blessed.box({
+        content: bigify(ended),
+        top: 0,
+        left: 0,
+        width: 11,
+        height: 5
+      });
+
+      container.append(finished);
+
+    }
+
+  } else {
+    // There is already o or x in that spot
+    cursor.style.fg = 'red';
   }
 
   screen.render();
@@ -135,3 +208,29 @@ screen.key('right', function(ch, key) {
 
 screen.render();
 
+var bigify = function (str) {
+  if (str === 'o') {
+    return '' +
+      '  .d88b.   \n' +
+      ' .8P  Y8.  \n' +
+      ' 88    88  \n' +
+      ' `8b  d8\' \n' +
+      '  `Y88P\'  \n';
+
+  } else if (str === 'x') {
+    return '' +
+      '  db   db  \n' +
+      '  `8b d8\' \n' +
+      '   `8d8\'  \n' +
+      '  .8P Y8.  \n' +
+      '  YP   YP  \n';
+
+  } else if (str === 'tie') {
+    return '' +
+      '──┬──   ┌──\n' +
+      '  │  ┬  │  \n' +
+      '  │  │  ├──\n' +
+      '  │  │  │  \n' +
+      '  │  ┴  └──\n';
+  }
+}
