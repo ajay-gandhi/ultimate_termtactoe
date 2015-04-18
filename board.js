e2e0850c0a43f2dc2c06704436be62af9d9c0a11
Ajay Gandhi <aag255@cornell.edu>
2015-04-17 22:37:32 -0400

    Finish app except endgame prompt

--- board.js
+++ board.js
@@ -2,8 +2,14 @@
 module.exports = (function () {
 
   function Board () {
+    // Mini boards
     this.boards = [];
     this.moves_on_board = [];
+
+    // Major board
+    this.major_board = [0, 1, 2, 3, 4, 5, 5, 6, 7, 8];
+    this.mini_wins = 0;
+
 
     // Create empty 9 x (3 x 3) array
     for (k = 0; k < 9; k++) {
@@ -17,7 +23,9 @@ module.exports = (function () {
   }
 
   Board.prototype.make_move = function (b, r, c, o) {
-    if (this.boards[b][r][c] === 'o' || this.boards[b][r][c] === 'x') {
+    var board = this.boards[b];
+
+    if (board[r][c] === 'o' || board[r][c] === 'x') {
       return false;
     } else {
       this.boards[b][r][c] = o;
@@ -33,24 +41,62 @@ module.exports = (function () {
     for (j = 0; j < 3; j++) {
       // Check row j
       if ((board[j][0] === board[j][1]) && (board[j][1] === board[j][2])) {
+        this.mini_wins++;
+        this.major_board[b] = board[j][0]; 
         return board[j][0];
       }
 
       // Check column j
       if ((board[0][j] === board[1][j]) && (board[1][j] === board[2][j])) {
+        this.mini_wins++;
+        this.major_board[b] = board[0][j]; 
         return board[0][j];
       }
     }
 
+    // Diagonals
     if ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
       (board[2][0] === board[1][1] && board[1][1] === board[0][2])) {
-      return board[0][0];
+      this.mini_wins++;
+      this.major_board[b] = board[1][1]; 
+      return board[1][1];
     }
 
-    if (this.moves_on_board[b] == 9)
+    if (this.moves_on_board[b] == 9) {
+      this.mini_wins++;
       return 'tie';
-    else
+
+    } else {
       return 'undetermined';
+    }
+  }
+
+  Board.prototype.won_game = function () {
+    var major = this.major_board;
+
+    for (j = 0; j < 3; j++) {
+
+      // Check row j
+      if ((major[j * 3] === major[j * 3 + 1]) && (major[j * 3] === major[j * 3 + 2])) {
+        return major[j * 3];
+      }
+
+      // Check column j
+      if ((major[j] === major[j + 3]) && (major[j] === major[j + 6])) {
+        return major[j];
+      }
+    }
+
+    // Diagonals
+    if ((major[0] === major[4] && major[0] === major[8]) ||
+      (major[2] === major[4] && major[2] === major[6])) {
+      return major[0];
+
+    } else if (this.mini_wins == 9) {
+      return 'tie';
+    } else {
+      return 'undetermined';
+    }
   }
 
   return Board;
--- game.js
+++ game.js
@@ -10,6 +10,7 @@ var cursor_position = {
   row: 0,
   col: 0
 }
+var finished_boards = [];
 
 var screen = blessed.screen({
   autoPadding: true,
@@ -37,7 +38,7 @@ var container = blessed.box({
     fg: 'white',
     bg: 'black',
     border: {
-      fg: '#f0f0f0'
+      fg: '#777777'
     }
   }
 });
@@ -82,117 +83,398 @@ var cursor = blessed.box({
 container.append(cursor);
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(['escape', 'q', 'C-c'], function() {
   return process.exit(0);
 });
 
 /* Key events */
 // Because of padding in container, get methods are offset
-screen.key('down', function(ch, key) {
+screen.key('down', function() {
   var t = cursor.top - 2;
   cursor.style.fg = 'white';
 
   if (t < 18) {
-    if (t == 4 || t == 11) {
-      cursor_position.board += 3;
-      cursor_position.row = 0;
+    if (finished_boards[cursor_position.board]) {
+      // Somewhere in a finished board
 
-      t++;
+      // Reset bg color of old cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#000000'
+        }
+      }
+
+      // Move cursor straight to next board
+      if (t <= 11) {
+        cursor.top = 14;
+        cursor_position.row = 0;
+        cursor_position.board += 3;
+
+        if (t <= 4) cursor.top = 7;
+
+      } else {
+        cursor.top = t + 2;
+        cursor_position.row++;
+      }
+
+      // Highlight current cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#333333'
+        }
+      }
+
+    } else {
+
+      // Regular move
+      if (t == 4 || t == 11) {
+
+        // Cursor is moving to a new board
+        cursor_position.board += 3;
+        cursor_position.row = -1;
+        t++;
+
+        // Change bg color of current cover
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#333333'
+          }
+        }
+      }
+
+      cursor_position.row++;
+      cursor.top = t + 2;
     }
-    cursor_position.row++;
-    cursor.top = t + 2;
   }
 
-  // console.log(cursor.top + ', ' + cursor.left);
+  // console.log(cursor_position.row + ', ' + cursor_position.col);
   screen.render();
 });
 
-screen.key('up', function(ch, key) {
+screen.key('up', function() {
   var t = cursor.top - 2;
   cursor.style.fg = 'white';
 
   if (t > 0) {
-    if (t == 7 || t == 14) {
-      cursor_position.board -= 3;
-      cursor_position.row = 2;
+    if (finished_boards[cursor_position.board]) {
+      // Somewhere in a finished board
 
-      t--;
+      // Reset bg color of old cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#000000'
+        }
+      }
+
+      // Move cursor straight to next
+      if (t >= 7) {
+        cursor.top = 4;
+        cursor_position.row = 2;
+        cursor_position.board -= 3;
+
+        if (t >= 14) cursor.top = 11;
+
+      } else {
+        cursor.top = t - 2;
+        cursor_position.row--;
+      }
+
+      // Highlight current cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#333333'
+        }
+      }
+
+    } else {
+  
+      // Regular move
+      if (t == 7 || t == 14) {
+        // Reset bg color of cover
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#000000'
+          }
+        }
+
+        // Cursor is moving to a new board
+        cursor_position.board -= 3;
+        cursor_position.row = 3;
+        t--;
+
+        finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#333333'
+          }
+        }
+      }
+
+      cursor_position.row--;
+      cursor.top = t - 2;
     }
-    cursor_position.row--;
-    cursor.top = t - 2;
   }
 
-  // console.log(cursor.top + ', ' + cursor.left);
+  // console.log(cursor_position.row + ', ' + cursor_position.col);
   screen.render();
 });
 
-screen.key('left', function(ch, key) {
+screen.key('left', function() {
   var l = cursor.left - 5;
   cursor.style.fg = 'white';
 
   if (l > 0) {
-    if (l == 14 || l == 28) {
-      cursor_position.board--;
-      cursor_position.col = 2;
+    if (finished_boards[cursor_position.board]) {
+      // Somewhere in a finished board
 
-      l -= 2;
+      // Reset bg color of old cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#000000'
+        }
+      }
+
+      // Move cursor straight to next
+      if (l >= 14) {
+        cursor.left = 9;
+        cursor_position.col = 2;
+        cursor_position.board--;
+
+        if (l >= 28)
+          cursor.left = 23;
+
+      } else {
+        cursor.left = l - 3;
+        cursor_position.col--;
+      }
+
+      // Highlight current cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#333333'
+        }
+      }
+
+    } else {
+
+      // Regular move
+      if (l == 14 || l == 28) {
+        // Reset bg color of cover
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#000000'
+          }
+        }
+
+        // Cursor is moving to a new board
+        cursor_position.board--;
+        cursor_position.col = 3;
+        l -= 2;
+
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#333333'
+          }
+        }
+      }
+
+      cursor_position.col--;
+      cursor.left = l - 3;
     }
-    cursor_position.col--;
-    cursor.left = l - 3;
   }
 
-  // console.log(cursor.top + ', ' + cursor.left);
+  // console.log(cursor_position.row + ', ' + cursor_position.col);
   screen.render();
 });
 
-screen.key('right', function(ch, key) {
+screen.key('right', function() {
   cursor.style.fg = 'white';
 
   var l = cursor.left - 5;
   if (l < 36) {
-    if (l == 8 || l == 22) {
-      cursor_position.board++;
-      cursor_position.col = 0;
-      l += 2;
+    if (finished_boards[cursor_position.board]) {
+      // Somewhere in a finished board
+
+      // Reset bg color of old cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#000000'
+        }
+      }
+
+      // Move cursor straight to next
+      if (l <= 22) {
+        cursor.left = 29;
+        cursor_position.col = 0;
+        cursor_position.board++;
+
+        if (l <= 8)
+          cursor.left = 15;
+
+      } else {
+        cursor.left = l + 5;
+        cursor_position.col++;
+      }
+
+      // Highlight current cover
+      var finished = finished_boards[cursor_position.board];
+      if (finished) {
+        finished.style = {
+          fg: finished.style.fg,
+          bg: '#333333'
+        }
+      }
+
+    } else {
+
+      // Regular move
+      if (l == 8 || l == 22) {
+        // Reset bg color of cover
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#000000'
+          }
+        }
+
+        // Cursor is moving to a new board
+        cursor_position.board++;
+        cursor_position.col = -1;
+        l += 2;
+
+        var finished = finished_boards[cursor_position.board];
+        if (finished) {
+          finished.style = {
+            fg: finished.style.fg,
+            bg: '#333333'
+          }
+        }
+      }
+
+      cursor_position.col++;
+      cursor.left = l + 5;
     }
-    cursor_position.col++;
-    cursor.left = l + 5;
   }
 
-  // console.log(cursor.top + ', ' + cursor.left);
+  // console.log(cursor_position.row + ', ' + cursor_position.col);
   screen.render();
 });
 
-screen.key('space', function(ch, key) {
-  var success = game.make_move(cursor_position.board, cursor_position.row, cursor_position.col, (o_turn) ? 'x' : 'o');
+screen.key('x', function() {
+  game_over = 'tie';
 
-  // Move was possible, i.e. spot empty
+  var over = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: 70,
+    height: 20,
+    content: ascii_winner(game_over),
+    align: 'center',
+    valign: 'bottom',
+    border: {
+      type: 'line',
+      fg: 'white'
+    },
+    style: {
+      fg: color_of(game_over),
+      bg: 'black'
+    }
+  });
+
+  container.append(over);
+
+  screen.render();
+});
+
+screen.key('space', function() {
+  // Ignore if completed
+  if (finished_boards[cursor_position.board]) return;
+
+  var c = (o_turn) ? 'x' : 'o';
+  var which = cursor_position.board;
+
+  var success = game.make_move(which, cursor_position.row, cursor_position.col, c);
+
+  // Move was possible, i.e. spot was empty
   if (success) {
+    // Add x or o to board
     var played = blessed.box({
-      content: (o_turn) ? 'x' : 'o',
+      content: c,
       top: cursor.top - 2,
       left: cursor.left - 4,
       width: 1,
       height: 1
     });
 
+    container.append(played);
+    played.setBack();
+
     o_turn = !o_turn;
 
-    container.append(played);
-    cursor.setFront();
+    var ended = game.won_board(which);
+    var game_over = game.won_game();
 
-    var ended = game.won_board(cursor_position.board);
-    console.log(ended);
+    if (game_over !== 'undetermined') {
 
-    if (ended !== 'undetermined') {
+      var over = blessed.box({
+        top: 'center',
+        left: 'center',
+        width: 70,
+        height: 20,
+        content: ascii_winner(game_over),
+        align: 'center',
+        valign: 'bottom',
+        border: {
+          type: 'line',
+          fg: 'white'
+        },
+        style: {
+          fg: 'white',
+          bg: 'black'
+        }
+      });
+
+      container.append(over);
+
+    } else if (ended !== 'undetermined') {
 
       var finished = blessed.box({
         content: bigify(ended),
-        top: 0,
-        left: 0,
+        top: (Math.floor(which / 3)) * 7,
+        left: (which % 3) * 14,
         width: 11,
-        height: 5
+        height: 5,
+        style: {
+          bg: '#333333'
+        }
       });
+
+      finished.style.fg = color_of(ended);
+      finished_boards[which] = finished;
 
       container.append(finished);
 
@@ -208,14 +490,19 @@ screen.key('space', function(ch, key) {
 
 screen.render();
 
+/**
+ * Converts a character to a large ASCII version.
+ * @param   {string} str The string to convert. One of [o, x, tie]
+ * @returns {string}     The ASCII-fied string
+ */
 var bigify = function (str) {
   if (str === 'o') {
     return '' +
-      '  .d88b.   \n' +
-      ' .8P  Y8.  \n' +
-      ' 88    88  \n' +
-      ' `8b  d8\' \n' +
-      '  `Y88P\'  \n';
+      '  .d888b.  \n' +
+      ' .8P   Y8. \n' +
+      ' 88     88 \n' +
+      ' `8b   d8\'\n' +
+      '  `Y888P\' \n';
 
   } else if (str === 'x') {
     return '' +
@@ -232,5 +519,78 @@ var bigify = function (str) {
       '  │  │  ├──\n' +
       '  │  │  │  \n' +
       '  │  ┴  └──\n';
+  }
+}
+
+/**
+ * Returns the color associated with a given player
+ * @param   {string} p The player
+ * @returns {string}   The color for player p
+ */
+var color_of = function (p) {
+  if (p === 'o') {
+    return '#00FFFF';
+
+  } else if (p === 'x') {
+    return '#FFA500';
+
+  } else {
+    return 'magenta';
+  }
+}
+
+/**
+ * Returns the ASCII art text for the given winner.
+ * @param   {string} winner The winner. One of [o, x, tie]
+ * @returns {string}        The ASCII string to display
+ */
+var ascii_winner = function (winner) {
+  var ascii_wins = '' +
+    ' ____      ____  _____  ____  _____   ______    \n' +
+    '|_  _|    |_  _||_   _||_   \\|_   _|.\' ____ \\  \n' +
+    '  \\ \\  /\\  / /    | |    |   \\ | |  | (___ \\_|  \n' +
+    '   \\ \\/  \\/ /     | |    | |\\ \\| |   _.____`.   \n' +
+    '    \\  /\\  /     _| |_  _| |_\\   |_ | \\____) |  \n' +
+    '     \\/  \\/     |_____||_____|\\____| \\______.\' \n';
+
+  var ascii_x = '' +
+    '  ____  ____  \n' +
+    ' |_  _||_  _| \n' +
+    '   \\ \\  / /   \n' +
+    '    > `\' <   \n' +
+    '  _/ /\'`\\ \\_ \n' +
+    ' |____||____| \n';
+
+  var ascii_o = '' +
+    '    ___     \n' +
+    '  .\'   `.  \n' +
+    ' /  .-.  \\  \n' +
+    ' | |   | |  \n' +
+    ' \\  `-\'  / \n' +
+    '  `.___.\'  \n';
+
+  var ascii_tie = '' +
+    '  _________  _____  ________  \n' +
+    ' |  _   _  ||_   _||_   __  | \n' +
+    ' |_/ | | \\_|  | |    | |_ \\_| \n' +
+    '     | |      | |    |  _| _  \n' +
+    '    _| |_    _| |_  _| |__/ | \n' +
+    '   |_____|  |_____||________| \n';
+
+  var binary_wins = '01010111 01001001 01001110 01010011';
+  var binary_o = '01101111';
+  var binary_x = '01111000';
+  var binary_tie = '01110100 01101001 01100101';
+
+  if (winner === 'tie') {
+    return ascii_tie + '\n\n\n\n\n\n' + binary_tie;
+
+  } else if (winner === 'o') {
+    return ascii_o + '\n' + ascii_wins + '\n\n\n' +
+      binary_o + ' ' + binary_wins;
+
+  } else if (winner === 'x') {
+    return ascii_x + '\n' + ascii_wins + '\n\n\n' +
+      binary_x + ' ' + binary_wins;
   }
 }
